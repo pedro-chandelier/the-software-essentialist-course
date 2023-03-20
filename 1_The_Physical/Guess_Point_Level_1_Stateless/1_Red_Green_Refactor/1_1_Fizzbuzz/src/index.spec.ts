b9edc4f1e2b1b1b1b633401f144da644cd919b9f
Pedro Haeser <haeser.pedro@gmail.com>
2023-03-20 00:40:11 -0300

    [refactor test] only accepts 1 to 100

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,8 +4,11 @@ describe("fizzbuzz", () => {
   it("returns a string", () => {
     expect(typeof fizzBuzz(5)).toBe("string");
     expect(fizzBuzz(1)).toBe("1");
-    expect(() => fizzBuzz(0)).toThrow()
-    expect(() => fizzBuzz(101)).toThrow()
-    expect(() => fizzBuzz(10)).not.toThrow()
+  });
+
+  it("accepts only 1 to 100", () => {
+    expect(() => fizzBuzz(0)).toThrow();
+    expect(() => fizzBuzz(101)).toThrow();
+    expect(() => fizzBuzz(10)).not.toThrow();
   });
 });
