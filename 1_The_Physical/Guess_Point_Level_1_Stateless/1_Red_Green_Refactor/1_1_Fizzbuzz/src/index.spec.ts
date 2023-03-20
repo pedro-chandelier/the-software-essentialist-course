c3f8ad2abd369590e2d99a964e80a5b96e150f7d
Pedro Haeser <haeser.pedro@gmail.com>
2023-03-20 00:39:18 -0300

    [test] only accepts 1 to 100

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,5 +4,8 @@ describe("fizzbuzz", () => {
   it("returns a string", () => {
     expect(typeof fizzBuzz(5)).toBe("string");
     expect(fizzBuzz(1)).toBe("1");
+    expect(() => fizzBuzz(0)).toThrow()
+    expect(() => fizzBuzz(101)).toThrow()
+    expect(() => fizzBuzz(10)).not.toThrow()
   });
 });
