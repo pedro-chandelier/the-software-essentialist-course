929f8b5eb7da732af6be62ab5dae5e5a0d2124a3
Pedro Haeser <haeser.pedro@gmail.com>
2023-03-20 00:33:00 -0300

    [test] returns number as string

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -3,5 +3,6 @@ import { fizzBuzz } from "./fizzbuzz";
 describe("fizzbuzz", () => {
   it("returns a string", () => {
     expect(typeof fizzBuzz(5)).toBe("string");
+    expect(fizzBuzz(1)).toBe("1");
   });
 });
