193941ee1541c9f018811b64b6fddd6686f44cb4
Pedro Haeser <haeser.pedro@gmail.com>
2023-03-20 00:48:03 -0300

    .

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -13,25 +13,25 @@ describe("fizzbuzz", () => {
   });
 
   it("returns Fizz for multiples of 3", () => {
-    expect(fizzBuzz(3)).toBe("Fizz")
-    expect(fizzBuzz(6)).toBe("Fizz")
-    expect(fizzBuzz(9)).toBe("Fizz")
-    expect(fizzBuzz(2)).toBe("2")
+    expect(fizzBuzz(3)).toBe("Fizz");
+    expect(fizzBuzz(6)).toBe("Fizz");
+    expect(fizzBuzz(9)).toBe("Fizz");
+    expect(fizzBuzz(2)).toBe("2");
   });
 
   it("returns Buzz for multiples of 5", () => {
-    expect(fizzBuzz(5)).toBe("Buzz")
-    expect(fizzBuzz(10)).toBe("Buzz")
-    expect(fizzBuzz(20)).toBe("Buzz")
-    expect(fizzBuzz(2)).toBe("2")
+    expect(fizzBuzz(5)).toBe("Buzz");
+    expect(fizzBuzz(10)).toBe("Buzz");
+    expect(fizzBuzz(20)).toBe("Buzz");
+    expect(fizzBuzz(2)).toBe("2");
   });
 
   it("returns FizzBuzz for multiples of 3 and 5", () => {
-    expect(fizzBuzz(15)).toBe("FizzBuzz")
-    expect(fizzBuzz(30)).toBe("FizzBuzz")
-    expect(fizzBuzz(45)).toBe("FizzBuzz")
-    expect(fizzBuzz(2)).toBe("2")
-    expect(fizzBuzz(3)).toBe("Fizz")
-    expect(fizzBuzz(5)).toBe("Buzz")
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+    expect(fizzBuzz(30)).toBe("FizzBuzz");
+    expect(fizzBuzz(45)).toBe("FizzBuzz");
+    expect(fizzBuzz(2)).toBe("2");
+    expect(fizzBuzz(3)).toBe("Fizz");
+    expect(fizzBuzz(5)).toBe("Buzz");
   });
 });
